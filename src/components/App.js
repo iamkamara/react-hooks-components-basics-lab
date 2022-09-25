e18f5bb48807d3097ab04154911a8529d3401fcf
onesmus kamara <onesmusqamara@gmail.com>
2022-09-25 18:32:31 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -7,24 +7,27 @@ function NavBar() {
     </nav>
   );
 }
-
 function Home() {
   return (
     <div id="home">
       <h1>Home</h1>
     </div>
   );
+  }
+
+function About(){
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
 }
-
-{/* write an <About> component here */}
-
-
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <NavBar/>
+      {<Home/>}
+      {<About/>}
     </div>
   );
 }
